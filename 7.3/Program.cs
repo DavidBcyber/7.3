0ee641dc2c4e98b3b5f4ac8725e41cd102bffc89
xbatm <xbatm@MSI>
2025-03-02 22:15:28 -0600

    Add project files.

---
+++ 7.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3
+{
+    internal class Program
+    {
+        class Product
+        {
+            public string productID { get; set; }
+            public string productName { get; set; }
+            public double price { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var prodductName = Product.Select(Product => Product);
+            Console.WriteLine(prodductName);
+
+            var removeproduct = string product.remove(string product => string product);
+            Console.WriteLine(removeproduct);
+
+            var calculateTotalPrice= price.calculate(price => price);
+            Console.WriteLine(calculateTotalPrice);
+        }
+    }
+}
